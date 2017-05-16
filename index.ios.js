46bc7e5512dad6f3edbb60fed41bbf87a9f0fcab
John Fritz <johnfritz@Johns-Mac.local>
2017-05-16 08:46:37 -0700

    downgrade to 0.43.3 to fix issue with react-router-flux

--- index.ios.js
+++ index.ios.js
@@ -15,7 +15,7 @@ import { authStatusReducer, graphqlStatusReducer } from './src/reducers/statusRe
 const client = new ApolloClient({
   connectToDevTools: true,
   networkInterface: createNetworkInterface({
-    uri: 'https://tlpm.ca/graphql',
+    uri: 'http://localhost:8080/graphql',
   },
     {
       shouldBatch: true,
